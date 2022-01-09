d0e04f678e11343447bdf8407e7e59634c495af2
KellyCoz <kelly.cozmanciuc@gmail.com>
2022-01-09 11:47:14 -0500

    Add project files.

---
+++ BeginnerApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BeginnerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
